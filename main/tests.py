1452ef706360527f5c55b9ca37ae931d53190b4c
David Fozo <fozo.david@gmail.com>
2016-11-09 16:16:49 +0100

    Article model is displayed on home page

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from main.views import home
+from main.models import Article
 
 # Create your tests here.
 class HomepageTest(TestCase):
@@ -8,3 +10,30 @@ class HomepageTest(TestCase):
     def test_root_resolves_to_home(self):
         root=resolve('/')
         self.assertEqual(root.func,home)
+
+    def test_root_loads_index_html(self):
+        request  = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<!doctype html>\n<html>\n<head>'))
+        self.assertIn("Hello World",response.content.decode())
+
+
+class ModelTest(TestCase):
+
+    def setUp(self):        
+        self.test_title = "TEST My Awesome Article TEST"
+        self.test_body  = "This is a text"
+        Article.objects.create(title=self.test_title, text=self.test_body)        
+        self.article = Article.objects.all().filter(title=self.test_title)
+        
+    def tearDown(self):
+        self.article.delete()
+        
+    def test_able_to_save_entries_to_db(self):
+        self.assertEqual(self.article.values()[0]['text'],self.test_body)
+
+    def test_index_html_displays_article(self):
+        request = HttpRequest()
+        response = home(request)
+        self.assertTrue(response.content.startswith(b'<!doctype html>\n<html>\n<head>'))
+        self.assertIn(self.test_body,response.content.decode())
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from main.models import Article
 
 # Create your views here.
 def home(request):
-    pass
+    article = Article.objects.last()
+    return render(request,'index.html',{'article':article,})
